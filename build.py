82f930a096933716e9d0f74439ea69b94c09df7e
wanghanyu1108@outlook.com <hanyu.wang1993@outlook.com>
2025-07-23 17:12:32 +0800

    build: 更新构建脚本中的文件路径
    - 将 "audio_tool/gui.py" 修改为 "./gui.py"
    - 将 "audio_tool/icon.ico" 修改为 "./icon.ico"- 将 "audio_tool/static/audio" 修改为 "./static/audio"
    
    这些修改统一了文件路径的表示方式，使用相对路径替代绝对路径，提高了代码的可移植性和可读性。

--- build.py
+++ build.py
@@ -2,11 +2,11 @@ import PyInstaller.__main__
 
 PyInstaller.__main__.run(
     [
-        "audio_tool/gui.py",
+        "./gui.py",
         "--onefile",
         "--noconsole",
-        "--icon=audio_tool/icon.ico",  # 可选
+        "--icon=./icon.ico",  # 可选
         "--name=TextToSpeechTool",
-        "--add-data=audio_tool/static/audio;audio_tool/static/audio",  # 保留音频目录
+        "--add-data=./static/audio;./static/audio",  # 保留音频目录
     ]
 )
